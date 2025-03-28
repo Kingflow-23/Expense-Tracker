4e62b4fa56f67ba2338bbaf3d4acfd9c7ccff7ca
KF23 <151558004+Kingflow-23@users.noreply.github.com>
2025-03-28 09:11:26 -0700

    Added models for user and expenses

---
+++ app/models/expense.py
@@ -0,0 +1,18 @@
+from datetime import datetime
+from sqlalchemy import Column, Integer, String, Float, DateTime, ForeignKey
+from sqlalchemy.orm import relationship
+
+from app.database.db import Base
+
+
+class Expense(Base):
+    __tablename__ = "expenses"
+
+    id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(Integer, ForeignKey("users.id"))
+    amount = Column(Float, nullable=False)
+    category = Column(String, nullable=False)
+    description = Column(String, nullable=True)
+    date = Column(DateTime, default=datetime.utcnow)
+
+    user = relationship("User", back_populates="expenses")
