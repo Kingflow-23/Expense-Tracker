7e59df56f8e91460d6657b3903daf9d5fc1df2c6
KF23 <151558004+Kingflow-23@users.noreply.github.com>
2025-03-28 09:16:42 -0700

    Added authentification feature

---
+++ app/auth/auth.py
@@ -0,0 +1,50 @@
+import os
+
+from jose import JWTError, jwt
+from dotenv import load_dotenv
+from fastapi import HTTPException
+from sqlalchemy.orm import Session
+from datetime import datetime, timedelta
+from passlib.context import CryptContext
+from fastapi.security import OAuth2PasswordBearer
+
+from app.models.user import User
+
+load_dotenv()
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/login")
+
+# Secret key and token settings
+SECRET_KEY = os.getenv("SECRET_KEY", "your_secret_key")
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 30))
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+
+def verify_password(plain_password: str, hashed_password: str) -> bool:
+    return pwd_context.verify(plain_password, hashed_password)
+
+
+def get_password_hash(password: str) -> str:
+    return pwd_context.hash(password)
+
+
+def create_access_token(data: dict, expires_delta: timedelta = None):
+    to_encode = data.copy()
+    expire = datetime.now(datetime.timezone.utc) + (
+        expires_delta if expires_delta else timedelta(minutes=15)
+    )
+    to_encode.update({"exp": expire})
+    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+
+
+def get_user_by_username(db: Session, username: str):
+    return db.query(User).filter(User.username == username).first()
+
+
+def authenticate_user(db: Session, username: str, password: str):
+    user = get_user_by_username(db, username)
+    if not user or not verify_password(password, user.hashed_password):
+        return None
+    return user
